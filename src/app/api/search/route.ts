c090a9056131960b0a1a485168eafe72e6aea8db
hnsreeny <hnsreeny@gmail.com>
2026-06-09 01:57:30 +0200

    fixing paths

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import { mediaItems } from "../../../lib/data";
-
+import { mediaItems } from "@/lib/data";
+// search using GET
 export async function GET(req: NextRequest) {
+    // getting the params
   const query =
     req.nextUrl.searchParams.get("q")?.toLowerCase() || "";
-
+// filtter 
   const results = mediaItems.filter((item) =>
     item.suchtext.toLowerCase().includes(query)
   );
-
+// json the resutls
   return NextResponse.json({
     items: results,
     total: results.length,
